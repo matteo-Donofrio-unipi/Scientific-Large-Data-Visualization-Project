f76a509349f944ca393c6b67440244d821ec308e
Matteo D'onofrio <69981848+matteo-Donofrio-unipi@users.noreply.github.com>
2022-01-20 16:24:44 +0100

    working plotly

--- show_data.py
+++ show_data.py
@@ -6,12 +6,21 @@ from os import sendfile
 from flask import (
     Blueprint, flash, redirect, render_template, url_for, request
 )
-from flask import Response, make_response
+#matlplot based
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import random
 import io
+import plotly as px
+
+#plotly based
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.io as pio
 
 #creo un blueprint chiamato auth
 #definisco url suo, che verrà preposto ad ogni view 
@@ -19,11 +28,12 @@ import io
 bp = Blueprint('show_data', __name__, url_prefix='/show_data')
 
 
-
 @bp.route('/<string:s>/show', methods=('GET', 'POST'))
 def show(s):
     return render_template('show_data/print.html', string_data=s)
 
+
+#---------------MATPLOTLIB/SEABORN BASED GRAPH-------#
 @bp.route('/plot')
 def plot():
     set={}
@@ -49,6 +59,30 @@ def create_figure():
     ys = [random.randint(1, 50) for x in xs]
     axis.plot(xs, ys)
     return fig
+
+def create_figure_plotly():
+    df = pd.DataFrame({
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    df2 = pd.DataFrame({
+        "Fruit": ["kiwi", "lime", "Bananas", "kiwi", "lime", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    })
+
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=False)
+    fig1 = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+    fig2 = px.bar(df2, x="Fruit", y="Amount", color="City", barmode="group")
+    fig1.update_xaxes(rangeslider_visible=True)
+    fig2.update_xaxes(rangeslider_visible=True)
+    
+    fig.add_trace(fig1['data'][0], row=1, col=1)
+    fig.add_trace(fig2['data'][0], row=2, col=1)
+
+    return fig
     
 
     #------------------------#
@@ -58,3 +92,16 @@ def create_figure():
     #render_template('page.html, result={'value_1': 'something 1', 'value_2': 'something 2'})
     #and in the template {{ result.value_1 }} and {{ result.value_2 }}
     #------------------------#
+
+
+    #---------------MATPLOTLIB/SEABORN BASED GRAPH-------#
+@bp.route('/plot2') 
+def plot2():
+    fig = create_figure_plotly()
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    #return render_template('show_data/print_plotly.html', graphJSON=graphJSON)
+    
+    return render_template('show_data/print_plotly.html', out=graphJSON)
+ 
+
+
